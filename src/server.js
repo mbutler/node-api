d09b1b825a82345035e7d3e8397b7a81bad1c9b3
Matthew Butler <matthewtbutler@gmail.com>
2021-05-21 15:14:19 -0500

    logging

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 //app.use(morgan("dev"))
 
-app.get("/data", (req, res) => {
+const log = (req, res, next) => {
+    console.log("logging")
+    next()
+}
+
+app.get("/data", log, (req, res) => {
     res.send({ message: "hey poopy" })
 })
 
